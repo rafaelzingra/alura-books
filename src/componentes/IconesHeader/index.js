281e2df204bf2940b0c0fabef26579a3cb25fd38
RafaelZingra <rafaelzingra@hotmail.com>
2024-02-04 01:41:11 -0300

    100% styled components

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,16 +1,26 @@
-import './estilo.css'
 import perfil from '../../imagens/perfil.svg'
 import sacola from '../../imagens/sacola.svg'
+import styled from 'styled-components'
+
+const Icones= styled.ul`
+    display: flex;
+    align-items: center;
+`
+
+const Icone= styled.li`    
+    margin-right: 40px;
+    width: 25px;  
+`
 
 const icones = [sacola, perfil]
 
 function IconesHeader () {
     return(
-        <ul className='icones'>
+        <Icones>
           { icones.map( (icone) => (
-            <li className='icone'><img src={icone}></img></li>
+            <Icone><img src={icone}></img></Icone>
           ) ) }
-        </ul>
+        </Icones>
     )
 }
 
