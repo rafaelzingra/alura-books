a279aa7d0cfe54bffe84a4556b608cad692f1125
RafaelZingra <rafaelzingra@hotmail.com>
2024-02-05 20:08:24 -0300

    add cards of latest releases

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './componentes/Header';
 import Pesquisa from './componentes/Pesquisa';
+import UltimosLancamentos from './componentes/UltimosLancamentos';
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
@@ -12,6 +13,7 @@ function App() {
     <AppContainer>
       <Header/>   
       <Pesquisa/>
+      <UltimosLancamentos/>
     </AppContainer> 
   );
 }
---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,9 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design',id: 1, src: livro },    
+    {nome: 'Cangaceiro em JavaScript: Uma aventura no sertão da programação',id: 2, src: livro },
+    {nome: 'Apache Kafka e Sprint Boot',id: 3, src: livro },
+]
+
+
